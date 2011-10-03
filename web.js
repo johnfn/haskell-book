cd3166229e25e33b05e5507655409f2f5897fa05
Grant Mathews <grant@causes.com>
2011-10-02 17:09:55 -0700

    Add JS for web.coffee.
    Trying to get the simplest possible thing running on Heroku. Later I
    will try to see if I can remove this js file from the repo if possible.
    I dislike having them - I feel they're kinda like object files.

---
+++ web.js
@@ -0,0 +1,41 @@
+(function() {
+  var BOOK_DIR, DEFAULT_PORT, app, book_parse, chapter_list, chapters, converter, express, fs, port, util;
+  express = require('express');
+  fs = require('fs');
+  converter = new (require('showdown')).Showdown.converter();
+  util = require('util');
+  DEFAULT_PORT = 3456;
+  BOOK_DIR = "chapters/";
+  chapter_list = fs.readdirSync(BOOK_DIR);
+  book_parse = function(list) {
+    var contents, file, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = list.length; _i < _len; _i++) {
+      file = list[_i];
+      contents = fs.readFileSync(BOOK_DIR + file, "UTF-8");
+      _results.push(converter.makeHtml(contents));
+    }
+    return _results;
+  };
+  chapters = book_parse(chapter_list);
+  app = express.createServer(express.logger());
+  app.set("view options", {
+    layout: false
+  });
+  app.set('view engine', 'coffee');
+  app.use(express.static(__dirname + "/public"));
+  app.register('.coffee', require('coffeekup').adapters.express);
+  app.get('/', function(request, response) {
+    return response.render('index', {
+      entries: chapter_list,
+      ch: chapters
+    });
+  });
+  app.get('/:id', function(request, response) {
+    return response.send(chapters[parseInt(request.params['id'])]);
+  });
+  port = process.env.PORT || DEFAULT_PORT;
+  app.listen(port, function() {
+    return console.log("Listening on http://localhost:" + port);
+  });
+}).call(this);
